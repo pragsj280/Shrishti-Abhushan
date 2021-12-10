fbfa033b19d62372b30227acd6a9f41d461bffff
Pragya jain <61374163+pragsj280@users.noreply.github.com>
2021-12-10 14:56:04 +0530

    Update settings.py

--- nurserynation/settings.py
+++ nurserynation/settings.py
@@ -139,9 +139,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # smtp configuration
 
-EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_PORT= 587
-EMAIL_USE_TLS= True
-EMAIL_HOST_USER='Shristisarvagya@gmail.com'
-EMAIL_HOST_PASSWORD= 'sjain5521'  
+#EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST= 'smtp.gmail.com'
+#EMAIL_PORT= 587
+#EMAIL_USE_TLS= True
+#EMAIL_HOST_USER='Shristisarvagya@gmail.com'
+#EMAIL_HOST_PASSWORD= 'sjain5521'  
